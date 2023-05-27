52abbd810003ccb343885c7f6f4339a724848f94
Giles Edkins <edkins@gmail.com>
2023-05-27 15:09:47 -0400

    Create TensorInfo from operator work in progress

--- Assets/Scripts/Onnx/OnnxHelper.cs
+++ Assets/Scripts/Onnx/OnnxHelper.cs
@@ -1,14 +1,23 @@
 using Google.Protobuf;
 using Onnx;
 using System.IO;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class OnnxHelper {
     public static void CreateModelProto(string modelPath) {
         var model = ModelProto.Parser.ParseFrom(File.OpenRead(modelPath));
+        var tensors = new Dictionary<string, TensorInfo>();
         Debug.Log("ModelProto created");
-        foreach (var valueinfo in model.Graph.ValueInfo) {
-            Debug.Log(valueinfo.Name);
+
+        foreach (var input in model.Graph.Input) {
+            tensors[input.Name] = TensorInfo.fromValueInfoProto(input, true);
+        }
+
+        foreach (var op in model.Graph.Node) {
+            for (int i = 0; i < op.Output.Count; i++) {
+                tensors[op.Output[i]] = TensorInfo.fromOperatorOutput(op, i, tensors);
+            }
         }
         Debug.Log("That's it for values");
     }
--- Assets/Scripts/Onnx/TensorInfo.cs
+++ Assets/Scripts/Onnx/TensorInfo.cs
@@ -1,12 +1,30 @@
 using Onnx;
+using System.Collections.Generic;
 
 public class TensorInfo {
     long[] d;
     int rank;
+    bool is_input;
 
-    static TensorInfo fromValueInfoProto(ValueInfoProto value) {
+    public static TensorInfo fromValueInfoProto(ValueInfoProto value, bool is_input) {
         var result = new TensorInfo();
         var dims = value.Type.TensorType.Shape.Dim;
+
+        result.is_input = is_input;
+        result.d = new long[dims.Count];
+        result.rank = dims.Count;
+        for (int i = 0; i < dims.Count; i++) {
+            result.d[i] = dims[i].DimValue;
+        }
+
+        return result;
+    }
+
+    public static TensorInfo fromOperatorOutput(NodeProto node, int output_index, Dictionary<string, TensorInfo> inputs) {
+        var result = new TensorInfo();
+        if (node.OpType == "conv" && output_index == 0) {
+            
+        }
         return result;
     }
 }
