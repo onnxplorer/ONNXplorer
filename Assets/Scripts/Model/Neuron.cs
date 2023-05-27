299a958eab0049d117da7415f50def4aad9d3e8b
Erhannis Kirran <eyeillus@gmail.com>
2023-05-27 13:21:20 -0400

    Committing loose changes before merge

---
+++ Assets/Scripts/Model/Neuron.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Neuron {
+    //DUMMY Rendering handle
+    Vector3 pos; //THINK Possibly need better res, with really big models
+    //DUMMY not done with this class yet
+}
